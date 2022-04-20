fc4f0b76182a025b3f6707fc251b4af72b311019
nagabhushan771 <naag000001@gmail.com>
2022-04-20 09:33:00 +0530

    Started trees

---
+++ dsa/src/dsa/Trees/TreeNode.java
@@ -0,0 +1,13 @@
+package dsa.Trees;
+
+public class TreeNode{
+	  int val;
+	  TreeNode left;
+	  TreeNode right;
+
+	  public TreeNode(int val){
+	    this.val = val;
+	    left = null;
+	    right = null;
+	  }
+	}
